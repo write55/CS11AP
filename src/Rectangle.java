08bcb720057d9379eec812d52b2d6be50ff040cd
write55 <writeusernamehere55@gmail.com>
2018-10-20 12:19:44 -0400

    asdfjkl;asdfjkl;

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -10,103 +10,103 @@ import java.io.InputStreamReader;
 
 public class Rectangle {
 
-	// CONSTANTS
-	public static final int MAX_WIDTH = 80;
-	public static final int MAX_HEIGHT = 25;
-	public static final int MIN = 1;
+    // CONSTANTS
+    public static final int MAX_WIDTH = 80;
+    public static final int MAX_HEIGHT = 25;
+    public static final int MIN = 1;
 
-	// PRIVATE DATA
-	private int width = 0;
-	private int height = 0;
+    // PRIVATE DATA
+    private int width = 0;
+    private int height = 0;
 
-	// CONSTRUCTOR
-	public Rectangle(int height, int width) {
-		this.height = height;
-		this.width = width;
-	}
+    // CONSTRUCTOR
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
 
-	// GETTERS
-	public int getHeight() {
-		return height;
-	}
+    // GETTERS
+    public int getHeight() {
+        return height;
+    }
 
-	public int getWidth() {
-		return width;
-	}
+    public int getWidth() {
+        return width;
+    }
 
-	// SETTERS
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    // SETTERS
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
-	public void setWidth(int width) {
-		this.width = width;
-	}
+    public void setWidth(int width) {
+        this.width = width;
+    }
 
-	// INPUTS AND ERROR TRAP
-	// First gets width and error traps
-	public void inputWidth() throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter width: ");
-		this.setWidth(new Integer(in.readLine()));
-		while (this.getWidth() > MAX_WIDTH || this.getWidth() < MIN) {
-			System.out.println("Width must be <= " + MAX_WIDTH + " and >= " + MIN);
-			System.out.print("Try again: ");
-			this.setWidth(new Integer(in.readLine()));
-		}
-	}
+    // INPUTS AND ERROR TRAP
+    // First gets width and error traps
+    public void inputWidth() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter width: ");
+        this.setWidth(new Integer(in.readLine()));
+        while (this.getWidth() > MAX_WIDTH || this.getWidth() < MIN) {
+            System.out.println("Width must be <= " + MAX_WIDTH + " and >= " + MIN);
+            System.out.print("Try again: ");
+            this.setWidth(new Integer(in.readLine()));
+        }
+    }
 
-	// Second gets height and error traps
-	public void inputHeight() throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter height: ");
-		this.setHeight(new Integer(in.readLine()));
-		while (this.getHeight() > MAX_HEIGHT || this.getHeight() < MIN) {
-			System.out.println("Height must be <= " + MAX_HEIGHT + " and >= " + MIN);
-			System.out.print("Try Again: ");
-			this.setHeight(new Integer(in.readLine()));
-		}
-	}
+    // Second gets height and error traps
+    public void inputHeight() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter height: ");
+        this.setHeight(new Integer(in.readLine()));
+        while (this.getHeight() > MAX_HEIGHT || this.getHeight() < MIN) {
+            System.out.println("Height must be <= " + MAX_HEIGHT + " and >= " + MIN);
+            System.out.print("Try Again: ");
+            this.setHeight(new Integer(in.readLine()));
+        }
+    }
 
-	// Just used a separate method to save room in main
-	public static char sentinelCheck() throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Would you like to continue? (Y/N)");
-		return in.readLine().charAt(0);
-	}
+    // Just used a separate method to save room in main
+    public static char sentinelCheck() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Would you like to continue? (Y/N)");
+        return in.readLine().charAt(0);
+    }
 
-	// toString also takes care of finding area then prints out l, w, a
-	public String toString() {
-		int w = this.getWidth();
-		int h = this.getHeight();
-		return "Your Rectangle is " + w + " stars across and " + h + " stars down, with an area of " + w * h;
-	}
+    // toString also takes care of finding area then prints out l, w, a
+    public String toString() {
+        int w = this.getWidth();
+        int h = this.getHeight();
+        return "Your Rectangle is " + w + " stars across and " + h + " stars down, with an area of " + w * h;
+    }
 
-	// Prints out rectangle according to l and w of the given object
-	public void printRectangle() {
-		int w = this.getWidth();
-		int h = this.getHeight();
-		for (int i = 1; i <= h; i++) {
-			for (int j = 1; j <= w; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		System.out.println(); // Extra space for output
-	}
+    // Prints out rectangle according to l and w of the given object
+    public void printRectangle() {
+        int w = this.getWidth();
+        int h = this.getHeight();
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= w; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println(); // Extra space for output
+    }
 
-	public static void main(String[] args) throws IOException {
-		Rectangle rect = new Rectangle(0, 0);
-		char sentinel = 'Y';
-		while (Character.toUpperCase(sentinel) == 'Y') {
-			rect.inputWidth();
-			rect.inputHeight();
-			System.out.println(rect.toString());
-			rect.printRectangle();
-			sentinel = sentinelCheck();
-		}
-		System.out.println("Program Terminated");
-	}
+    public static void main(String[] args) throws IOException {
+        Rectangle rect = new Rectangle(0, 0);
+        char sentinel = 'Y';
+        while (Character.toUpperCase(sentinel) == 'Y') {
+            rect.inputWidth();
+            rect.inputHeight();
+            System.out.println(rect.toString());
+            rect.printRectangle();
+            sentinel = sentinelCheck();
+        }
+        System.out.println("Program Terminated");
+    }
 }
 
 // OUTPUT - format this better a bit
--- src/Registration.java
+++ src/Registration.java
@@ -5,12 +5,66 @@ Aaron Wu
 public class Registration {
 
     // CONSTANTS
+    public static final int BATIK_LIMIT = 6;
+    public static final int CALIGRAPHY_LIMIT = 4;
+    public static final int PAINTING_LIMIT = 7;
+    public static final int SCULTURE_LIMIT = 4;
+    public static final int WEAVING_LIMIT = 5;
+    public static final int SENTINEL = 0;
+    public static final int CLASS_LIMIT = 2;
 
     // PRIVATE DATA
+    private int batik = 0;
+    private int caligraphy = 0;
+    private int painting = 0;
+    private int sculpture = 0;
+    private int weaving = 0;
 
     // CONSTRUCTOR (No Arguments)
     public Registration() {
 
+    }
+
+    // GETTERS
+    public int getBatik() {
+        return batik;
+    }
+
+    public int getCaligraphy() {
+        return caligraphy;
+    }
+
+    public int getPainting() {
+        return painting;
+    }
+
+    public int getSculpture() {
+        return sculpture;
+    }
+
+    public int getWeaving() {
+        return weaving;
+    }
+
+    // SETTERS
+    public void setBatik(int batik) {
+        this.batik = batik;
+    }
+
+    public void setCaligraphy(int caligraphy) {
+        this.caligraphy = caligraphy;
+    }
+
+    public void setPainting(int painting) {
+        this.painting = painting;
+    }
+
+    public void setSculpture(int sculpture) {
+        this.sculpture = sculpture;
+    }
+
+    public void setWeaving(int weaving) {
+        this.weaving = weaving;
     }
 
 
