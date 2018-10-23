78919348f0a53b882cb3dd0a7b4ddd8bb531234c
write55 <42893610+write55@users.noreply.github.com>
2018-10-23 10:53:14 -0400

    Update Registration.java

--- src/Registration.java
+++ src/Registration.java
@@ -12,124 +12,131 @@ import java.io.InputStreamReader;
 
 public class Registration {
 
-    // CONSTANTS
-    public static final int BATIK_LIMIT = 6;
-    public static final int CALIGRAPHY_LIMIT = 4;
-    public static final int PAINTING_LIMIT = 7;
-    public static final int SCULTURE_LIMIT = 4;
-    public static final int WEAVING_LIMIT = 5;
-    public static final int SENTINEL = 0;
-    public static final int SECTION_LIMIT = 2;
+	// CONSTANTS
+	public static final int BATIK_LIMIT = 6;
+	public static final int CALIGRAPHY_LIMIT = 4;
+	public static final int PAINTING_LIMIT = 7;
+	public static final int SCULPTURE_LIMIT = 4;
+	public static final int WEAVING_LIMIT = 5;
+	public static final int SENTINEL = 0;
+	public static final int SECTION_LIMIT = 2;
 
-    // PRIVATE DATA
-    private int batik = 0;
-    private int caligraphy = 0;
-    private int painting = 0;
-    private int sculpture = 0;
-    private int weaving = 0;
+	// PRIVATE DATA
+	private int batik = 0;
+	private int caligraphy = 0;
+	private int painting = 0;
+	private int sculpture = 0;
+	private int weaving = 0;
 
-    // CONSTRUCTOR (No Arguments)
-    public Registration() {
-        batik = 0;
-        caligraphy = 0;
-        painting = 0;
-        sculpture = 0;
-        weaving = 0;
-    }
+	// CONSTRUCTOR (No Arguments)
+	public Registration() {
+	}
 
-    // GETTERS
-    public int getBatik() {
-        return batik;
-    }
+	// INPUT METHODS
 
-    public int getCaligraphy() {
-        return caligraphy;
-    }
+	// First is for ID
+	public static int inputId() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter your ID Number: ");
+		int id = new Integer(in.readLine());
+		return id;
+	}
 
-    public int getPainting() {
-        return painting;
-    }
+	// Second is for Course code, also error traps
+	public static char inputCourseCode() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter your Course Code: ");
+		// Makes Sure course code is always uppercase
+		char course = Character.toUpperCase(in.readLine().charAt(0));
+		while (course != 'B' && course != 'C' && course != 'P' && course != 'S' && course != 'W') {
+			System.out.println("Course character doesn't match with any available courses");
+			System.out.print("Try Again: ");
+			course = Character.toUpperCase(in.readLine().charAt(0));
+		}
+		return course;
+	}
 
-    public int getSculpture() {
-        return sculpture;
-    }
+	// CHANGE COURSE CODE TO FULL NAME
+	public static String getCourseName(char input) {
+		String name;
+		if (input == 'B') {
+			name = "Batik";
+		} else if (input == 'C') {
+			name = "Caligraphy";
+		} else if (input == 'P') {
+			name = "Painting";
+		} else if (input == 'S') {
+			name = "Sculpture";
+		} else {
+			name = "Weaving";
+		}
+		return name;
+	}
 
-    public int getWeaving() {
-        return weaving;
-    }
+	// TOSTRING FOR NUMBER OF STUDENTS IN EACH COURSE
+	public String toString() {
+		return "Final Course Counts:" + "\nBatik: " + this.batik + "\nCaligraphy: " + this.caligraphy + "\nPainting: "
+				+ this.painting + "\nSculpture: " + this.sculpture + "\nWeaving: " + this.weaving;
+	}
 
-    // SETTERS
-    public void setBatik(int batik) {
-        this.batik = batik;
-    }
+	// ADD STUDENT TO GIVEN CLASS
+	public void setClassCounts(char input) {
+		if (input == 'B') {
+			this.batik++;
+		} else if (input == 'C') {
+			this.caligraphy++;
+		} else if (input == 'P') {
+			this.painting++;
+		} else if (input == 'S') {
+			this.sculpture++;
+		} else {
+			this.weaving++;
+		}
+	}
 
-    public void setCaligraphy(int caligraphy) {
-        this.caligraphy = caligraphy;
-    }
+	// Old - Delete This
+	public int getSections(char input) {
+		int sections = 0;
+		int limit = 0;
+		int students = 0;
+		if (input == 'B') {
+			limit = BATIK_LIMIT;
+			students = this.batik;
+		} else if (input == 'C') {
+			limit = CALIGRAPHY_LIMIT;
+			students = this.caligraphy;
+		} else if (input == 'P') {
+			limit = PAINTING_LIMIT;
+			students = this.painting;
+		} else if (input == 'S') {
+			limit = SCULPTURE_LIMIT;
+			students = this.sculpture;
+		} else {
+			limit = WEAVING_LIMIT;
+			students = this.weaving;
+		}
+		if (students > 2) {
+			while (students > 1) {
+				students -= limit;
+				sections++;
+			}
+		}
+		return sections;
+	}
 
-    public void setPainting(int painting) {
-        this.painting = painting;
-    }
+	public static void main(String[] args) throws IOException {
+		Registration info = new Registration();
+		int id = inputId();
+		while (id != 0) {
+			char course = inputCourseCode();
+			System.out.println("Added to given course" + "\n" + "ID Number: " + id + "\n" + "Course taken: "
+					+ getCourseName(course));
 
-    public void setSculpture(int sculpture) {
-        this.sculpture = sculpture;
-    }
+			id = inputId();
+			info.setClassCounts(course);
+		}
+		System.out.println("Program ended");
+		System.out.println("\n" + info.toString());
 
-    public void setWeaving(int weaving) {
-        this.weaving = weaving;
-    }
-
-    // INPUT - takes care of error trapping course code
-    public static String getInput() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter ID and first letter of course (capitalized) separated by a space: ");
-        String input = in.readLine();
-        // int id = new Integer(input.substring(0, input.indexOf(" ") - 1));
-        char course = Character.toUpperCase(input.charAt(input.length() - 1));
-        while (course != 'B' && course != 'C' && course != 'P' && course != 'S' && course != 'W') {
-            System.out.println("Course character doesn't match with any available courses");
-            System.out.print("Try Again: ");
-            input = in.readLine();
-            course = Character.toUpperCase(input.charAt(input.length() - 1));
-        }
-        return input;
-    }
-
-    public static String getCourseName(char c) {
-        String name;
-        switch (c) {
-            case 'B':
-                name = "Batik";
-            case 'C':
-                name = "Caligraphy";
-            case 'P':
-                name = "Painting";
-            case 'S':
-                name = "Sculpture";
-            case 'W':
-                name = "Weaving";
-            default:
-                name = " ";
-        }
-        return name;
-    }
-
-    public String toString() {
-        return null;
-    }
-
-    public void setClassCounts() {
-
-    }
-
-    public int getSections() {
-        int sections = 0;
-        return sections;
-    }
-
-    public static void main(String[] args) throws IOException {
-        Registration info = new Registration();
-        String student = info.getInput();
-    }
-
+	}
 }
