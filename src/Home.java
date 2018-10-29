fa52c87a1fe54e89868e2b200485ebfca2967e59
write55 <writeusernamehere55@gmail.com>
2018-10-29 17:59:27 -0400

    Added some stuff to Home.java

--- src/Home.java
+++ src/Home.java
@@ -1,3 +1,9 @@
+/*
+Aaron Wu
+10/29/18
+Program to do something with house values and stuff - change this later
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,35 +29,35 @@ public class Home {
     }
 
     // INPUT METHODS
-    // Money, error traps for positive integer - delete this
-    public void inputMoney() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter your budget as a positive integer: ");
-        int i = Integer.parseInt(in.readLine());
-        // Check for negative value
-        while (i <= 0) {
-            System.out.print("Value not positive\nTry Again: ");
-            i = Integer.parseInt(in.readLine());
-        }
-        this.money = i;
-    }
 
     /*
-     * General method for l/w/f and money, checks for positive integer, no error
-     * trap for integer since it'll crash the program if you enter a decimal so
-     * it's not needed (no NumberFormatException)
+     General method for l/w/f and money, checks for positive integer, no error
+     trap for integer since it'll crash the program if you enter a decimal so
+     it's not needed (no NumberFormatException)
      */
     public static int inputValue() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int i = Integer.parseInt(in.readLine());
         while (i <= 0) {
-            System.out.print("Value not positive\nTry Again: ");
+            System.out.print("Value is zero or not positive\nTry Again: ");
             i = Integer.parseInt(in.readLine());
         }
         return i;
     }
 
-    // Method for style
+    // Method to run number private var input methods to save room in main for other stuff
+    public void inputValuesRunner() throws IOException {
+        System.out.print("Enter budget as a positive integer: ");
+        this.money = inputValue();
+        System.out.print("Enter length: ");
+        this.length = inputValue();
+        System.out.print("Enter width: ");
+        this.width = inputValue();
+        System.out.print("Enter number of floors: ");
+        this.floors = inputValue();
+    }
+
+    // Method for style - separate since it takes char not integer
     public void inputStyle() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter your house's style code: ");
@@ -67,13 +73,13 @@ public class Home {
     public static String convertStyle(char c) {
         String output;
         if (c == 'M') {
-            output = "minimum";
+            output = "Minimum";
         } else if (c == 'S') {
-            output = "standard";
+            output = "Standard";
         } else if (c == 'E') {
-            output = "energy-efficient";
+            output = "Energy-Efficient";
         } else {
-            output = "custom";
+            output = "Custom";
         }
         return output;
     }
@@ -81,14 +87,16 @@ public class Home {
     // Converts private data to string
     public String toString() {
         String style = convertStyle(this.style);
-        return "Cost: " + this.money + "\nLength: " + this.length + "\nWidth: " + this.width + "\nFloors: "
+        return "Information for your house:\nCost: " + this.money + "\nLength: " + this.length + "\nWidth: " + this.width + "\nFloors: "
                 + this.floors + "\nStyle: " + style;
     }
 
     public static void main(String[] args) throws IOException {
         Home house = new Home();
-        System.out.print("Enter budget as a positive integer: ");
-        house.money = inputValue();
+        house.inputValuesRunner();
+        house.inputStyle();
+        System.out.println("\n" + house.toString());
+
     }
 
 }
