92dc9871e71e99517ff36040a4bf09b295781046
write55 <42893610+write55@users.noreply.github.com>
2018-11-07 10:51:20 -0500

    Add files via upload

---
+++ src/Palindrome.java
@@ -0,0 +1,52 @@
+
+/*
+Aaron Wu
+11/7/18
+Program to take user input for a string and check if it's a palindrome, loops until the user enters 0
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Palindrome {
+
+	public static boolean palindromeCheck(String str) {
+		int left = 0;
+		int right = str.length() - 1;
+		while (right - left > 0) {
+			while (Character.isLetterOrDigit(str.charAt(left)) == false) {
+				left++;
+			}
+			while (Character.isLetterOrDigit(str.charAt(right)) == false) {
+				right--;
+			}
+
+			if (Character.toLowerCase(str.charAt(left)) != Character.toLowerCase(str.charAt(right))) {
+				return false;
+			} else {
+				left++;
+				right--;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("The program will print out OK for a Palindrome and X for not a palindrome."
+				+ "\nEnter 0 to exit the program.");
+		System.out.print("Enter a String: ");
+		String input = in.readLine();
+		while (input != "0") {
+			if (palindromeCheck(input) == true) {
+				System.out.println("OK");
+			} else {
+				System.out.println("X");
+			}
+			System.out.print("Enter a new string: ");
+			input = in.readLine();
+		}
+	}
+
+}
