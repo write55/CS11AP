b5aacd3bb08d2d852b4c1013025071ad06f38881
write55 <writeusernamehere55@gmail.com>
2018-11-17 09:58:27 -0500

    Edited Palindrome Program

--- src/Tally.java
+++ src/Tally.java
@@ -6,37 +6,38 @@ import java.io.InputStreamReader;
 
 public class Tally {
 
-	public Tally() {
+    public Tally() {
 
-	}
+    }
 
-	private int[] letters = new int[26];
-	private int total = 0;
+    private int[] letters = new int[26];
+    private int total = 0;
 
-	public String inputFile() throws IOException { // This doesn't work
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter filename with extension, must be in same directory as program\nEnter: ");
-		FileReader readFile = new FileReader(in.readLine());
-		BufferedReader inFile = new BufferedReader(readFile);
-		System.out.println("Reading File...");
-		String input = inFile.readLine();
-		while (input != null) {
-			System.out.println(input);
-			input = inFile.readLine();
-		}
-		inFile.close();
-		return input;
-	}
+    public String inputFile() throws IOException { // This doesn't work
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter filename with extension, must be in same directory as program\nEnter: ");
+        FileReader readFile = new FileReader(in.readLine());
+        BufferedReader inFile = new BufferedReader(readFile);
+        System.out.println("Reading File...");
+        String input = inFile.readLine();
+        while (input != null) {
+            System.out.println(input);
+            input = inFile.readLine();
+        }
+        inFile.close();
+        return input;
+    }
 
-	public void convertLetter(String file) {
-		for (int i = 0; i < file.length() - 1; i++) {
-			letters[Character.toUpperCase(file.charAt(i)) - 'A']++;
-		}
-	}
+    public void convertLetter(String file) {
+        for (int i = 0; i < file.length(); i++) {
+            System.out.println(Character.toUpperCase(file.charAt(i)) - 'A');
+        }
 
-	public static void main(String[] args) throws IOException {
-		Tally file = new Tally();
-		file.convertLetter("Aa Bb Cc Dd Ee Ff Gg");
+    }
 
-	}
+    public static void main(String[] args) throws IOException {
+        Tally file = new Tally();
+        file.convertLetter("Aa Bb Cc Dd Ee Ff Gg Zz");
+
+    }
 }
