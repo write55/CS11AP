1133a3dd14517631c4040ff6c52d74a8547c623b
write55 <writeusernamehere55@gmail.com>
2018-11-17 10:20:41 -0500

    Edited Palindrome Program

--- src/Tally.java
+++ src/Tally.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Tally {
 
@@ -12,6 +13,14 @@ public class Tally {
 
     private int[] letters = new int[26];
     private int total = 0;
+
+    public int[] getLetters() {
+        return letters;
+    }
+
+    public int getTotal() {
+        return total;
+    }
 
     public String inputFile() throws IOException { // This doesn't work
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -30,9 +39,12 @@ public class Tally {
 
     public void convertLetter(String file) {
         for (int i = 0; i < file.length(); i++) {
-            System.out.println(Character.toUpperCase(file.charAt(i)) - 'A');
+            // If character returns less than 65 it's not a letter
+            if ((int) file.charAt(i) > 64) {
+                this.letters[(int) Character.toUpperCase(file.charAt(i)) - 65]++;
+                this.total++;
+            }
         }
-
     }
 
     public static void main(String[] args) throws IOException {
