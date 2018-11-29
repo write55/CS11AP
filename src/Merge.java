c6ec53783b179811070f28da4fd7c788cd225474
write55 <42893610+write55@users.noreply.github.com>
2018-11-29 10:53:27 -0500

    Update Merge.java

--- src/Merge.java
+++ src/Merge.java
@@ -2,7 +2,7 @@
 /*
 Aaron Wu
 11/27/18
-Program to do IDK yet
+TODO finish description 
  */
 
 import java.io.BufferedReader;
@@ -12,40 +12,98 @@ import java.util.StringTokenizer;
 
 // check element on one list to one on other list, increase position for lesser one and append to final list
 // if duplicates are present just ignore them, don't put the condition in the if
-// use that for the final, sort both first
+// use that for the final, sort both first - do insertion sort maybe
 // merge has 1 array with length, have 3 objects
 
 public class Merge {
 
+	// PRIVATE DATA
 	private int[] array;
 	private int filled;
 
-	public Merge(int length, int filled) {
+	// TODO Add field for filled on 3rd array
+	// CONSTRUCTOR - only argument for array length
+	public Merge(int length) {
 		array = new int[length];
-		this.filled = filled;
 	}
 
+	// GETTERS - probably won't need
+	public int[] getArray() {
+		return array;
+	}
+
+	public int getFilled() {
+		return filled;
+	}
+
+	// Fills array and gives number filled
 	public void inputArray() throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter a string of numbers separated by spaces");
-		String temp = in.readLine();
-
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		for (filled = 0; filled < array.length && st.hasMoreTokens(); filled++) {
+			array[filled] = Integer.parseInt(st.nextToken());
+		}
 	}
 
-	public void sortArray() {
-
-	}
-
+	// Prints out array in proper format from an object
 	public void printArray(int number) {
 		System.out.print("List " + number + "--> ");
 		for (int i = 0; i < filled; i++) {
 			System.out.print(array[i] + " ");
 		}
+		System.out.println();
 	}
 
-	public static int[] mergeArrays(int[] X, int[] Y) {
-		int[] Z = new int[X.length + Y.length];
-		return Z;
+	// TODO Sort Method
+	// Sorts arrays, for use on first and second
+	public void sortArray() {
+		// Insertion Sort (From slides)
+		for (int j = 1; j < filled; j++) {
+			int temp = array[j];
+			int possible = j;
+			while (possible > 0 && temp < array[possible - 1]) {
+				array[possible] = array[possible - 1];
+				possible--;
+			}
+			array[possible] = temp;
+		}
+	}
+
+	// TODO Merge Method
+	public void mergeArrays(Merge X, Merge Y) {
+		int c1 = 0, c2 = 0;
+		for (int i = 0; i < this.array.length; i++) {
+			while (c1 < X.filled && c2 < Y.filled) {
+				if (X.array[c1] < Y.array[c2]) {
+					this.array[i] = X.array[c1];
+					c1++;
+				} else {
+					this.array[i] = Y.array[c2];
+					c2++;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		Merge first = new Merge(15);
+		Merge second = new Merge(15);
+		System.out.println("Enter a string of numbers separated by spaces");
+		System.out.println("If your string is greater than 15 excess values will not be stored");
+		System.out.print("First Array: ");
+		first.inputArray();
+		System.out.print("Second Array: ");
+		second.inputArray();
+		System.out.println("\nSorted arrays:");
+		first.sortArray();
+		first.printArray(1);
+		second.sortArray();
+		second.printArray(2);
+		System.out.println("\nMerged Array:");
+		Merge combined = new Merge(first.getFilled() + second.getFilled());
+		combined.mergeArrays(first, second);
+		combined.printArray(3);
+
 	}
 
 }
