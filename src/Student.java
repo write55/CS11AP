8760c0ac3711eda8e3fe8a1cc47b933dad353094
write55 <writeusernamehere55@gmail.com>
2018-12-06 18:08:32 -0500

    Updated Student Program

--- src/Student.java
+++ src/Student.java
@@ -1,81 +1,81 @@
 
 public class Student {
 
-	// CONSTANTS
-	public static final char code1 = 'C';
-	public static final String course1 = "Computer Science";
-	public static final char code2 = 'E';
-	public static final String course2 = "Education";
-	public static final char code3 = 'M';
-	public static final String course3 = "Mathematics";
-	public static final char code4 = 'B';
-	public static final String course4 = "Business";
-	public static final char code5 = 'N';
-	public static final String course5 = "Engineering";
-	public static final char code6 = 'A';
-	public static final String course6 = "Art";
+    // CONSTANTS
+    public static final char code1 = 'C';
+    public static final String course1 = "Computer Science";
+    public static final char code2 = 'E';
+    public static final String course2 = "Education";
+    public static final char code3 = 'M';
+    public static final String course3 = "Mathematics";
+    public static final char code4 = 'B';
+    public static final String course4 = "Business";
+    public static final char code5 = 'N';
+    public static final String course5 = "Engineering";
+    public static final char code6 = 'A';
+    public static final String course6 = "Art";
 
-	// PRIVATE DATA
-	private int id;
-	private int score;
-	private char course;
+    // PRIVATE DATA
+    private int id;
+    private int score;
+    private char course;
 
-	// CONSTRUTOR
-	public Student(int id, int score, char course) {
-		this.id = id;
-		this.score = score;
-		this.course = course;
-	}
+    // CONSTRUTOR
+    public Student(int id, int score, char course) {
+        this.id = id;
+        this.score = score;
+        this.course = course;
+    }
 
-	// GETTERS
-	public int getId() {
-		return id;
-	}
+    // GETTERS
+    public int getId() {
+        return id;
+    }
 
-	public int getScore() {
-		return score;
-	}
+    public int getScore() {
+        return score;
+    }
 
-	public char getCourse() {
-		return course;
-	}
+    public char getCourse() {
+        return course;
+    }
 
-	// SETTERS
-	public void setId(int id) {
-		this.id = id;
-	}
+    // SETTERS
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setScore(int score) {
-		this.score = score;
-	}
+    public void setScore(int score) {
+        this.score = score;
+    }
 
-	public void setCourse(char course) {
-		this.course = course;
-	}
+    public void setCourse(char course) {
+        this.course = course;
+    }
 
-	// Convert code to full string, doesn't use private data so static
-	public static String convertCode(char code) {
-		switch (code) {
-		case code1:
-			return course1;
-		case code2:
-			return course2;
-		case code3:
-			return course3;
-		case code4:
-			return course4;
-		case code5:
-			return course5;
-		case code6:
-			return course6;
-		default:
-			return "Incorrect Course Code";
-		}
-	}
+    // Convert code to full string, doesn't use private data so static
+    public static String convertCode(char code) {
+        switch (code) {
+            case code1:
+                return course1;
+            case code2:
+                return course2;
+            case code3:
+                return course3;
+            case code4:
+                return course4;
+            case code5:
+                return course5;
+            case code6:
+                return course6;
+            default:
+                return "Incorrect Course Code";
+        }
+    }
 
-	// TOSTRING
-	public String toString() {
-		return "Id: " + getId() + "\nScore: " + getScore() + "\nCourse: " + convertCode(getCourse());
-	}
+    // TOSTRING
+    public String toString() {
+        return "Id: " + getId() + "\nScore: " + getScore() + "\nCourse: " + convertCode(getCourse());
+    }
 
 }
--- src/StudentArray.java
+++ src/StudentArray.java
@@ -6,60 +6,60 @@ import java.util.StringTokenizer;
 
 public class StudentArray {
 
-	// CONSTANTS
-	public static final int studentMax = 30;
-	public static final int gradeMin = 0;
-	public static final int gradeMax = 100;
+    // CONSTANTS
+    public static final int studentMax = 30;
+    public static final int gradeMin = 0;
+    public static final int gradeMax = 100;
 
-	// PRIVATE DATA
-	private Student[] array = new Student[studentMax];
-	private int filled;
-	private double mean;
-	private int median;
-	private int highest;
-	private int lowest;
+    // PRIVATE DATA
+    private Student[] array = new Student[studentMax];
+    private int filled;
+    private double mean;
+    private int median;
+    private int highest;
+    private int lowest;
 
-	// NO ARGUMENT CONSTRUCTOR
-	public StudentArray() {
+    // NO ARGUMENT CONSTRUCTOR
+    public StudentArray() {
 
-	}
+    }
 
-	// FILE READER
-	public void readFile() throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter name of file, file must be in project directory\nEnter: ");
-		FileReader readFile = new FileReader(in.readLine());
-		BufferedReader inFile = new BufferedReader(readFile);
-		System.out.println("\nReading File...\n");
-		String input = inFile.readLine();
-		while (input != null) {
-			tokenizeString(input);
-			System.out.println(input);
-			input = inFile.readLine();
-		}
-		inFile.close();
-	}
+    // FILE READER
+    public void readFile() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter name of file, file must be in project directory\nEnter: ");
+        FileReader readFile = new FileReader(in.readLine());
+        BufferedReader inFile = new BufferedReader(readFile);
+        System.out.println("\nReading File...\n");
+        String input = inFile.readLine();
+        while (input != null) {
+            tokenizeString(input);
+            System.out.println(input);
+            input = inFile.readLine();
+        }
+        inFile.close();
+    }
 
-	// Takes 3 values from string, inputs them into the student array
-	// called in file reader method
-	public void tokenizeString(String input) throws IOException {
-		StringTokenizer st = new StringTokenizer(input);
-		for (filled = 0; filled < array.length && st.hasMoreTokens(); filled++) {
-			array[filled] = new Student(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
-					st.nextToken().charAt(0));
-		}
-	}
+    // Takes 3 values from string, inputs them into the student array
+    // called in file reader method
+    public void tokenizeString(String input) {
+        StringTokenizer st = new StringTokenizer(input);
+        for (filled = 0; filled < array.length && st.hasMoreTokens(); filled++) {
+            array[filled] = new Student(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                    st.nextToken().charAt(0));
+        }
+    }
 
-	// Prints array, called "arrayToString" to distinguish toString in Student
-	public void arrayToString() {
-		for (int i = 0; i < filled; i++) {
-			System.out.println(array[i].toString());
-		}
-	}
+    // Prints array, called "arrayToString" to distinguish toString in Student
+    public void arrayToString() {
+        for (int i = 0; i < filled; i++) {
+            System.out.println(array[i].toString());
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		StudentArray roster = new StudentArray();
-		roster.readFile();
-		roster.arrayToString();
-	}
+    public static void main(String[] args) throws IOException {
+        StudentArray roster = new StudentArray();
+        roster.readFile();
+        roster.arrayToString();
+    }
 }
