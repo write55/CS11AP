0ec43cd662ade5cac81cedecfac202f50ec973e7
write55 <42893610+write55@users.noreply.github.com>
2018-12-12 10:52:37 -0500

    Add files via upload

--- src/Student.java
+++ src/Student.java
@@ -1,76 +1,76 @@
 
 public class Student {
 
-    // CONSTANTS
-    public static final char code1 = 'C';
-    public static final String course1 = "Computer Science";
-    public static final char code2 = 'E';
-    public static final String course2 = "Education";
-    public static final char code3 = 'M';
-    public static final String course3 = "Mathematics";
-    public static final char code4 = 'B';
-    public static final String course4 = "Business";
-    public static final char code5 = 'N';
-    public static final String course5 = "Engineering";
-    public static final char code6 = 'A';
-    public static final String course6 = "Art";
+	// CONSTANTS
+	public static final char code1 = 'C';
+	public static final String course1 = "Computer Science";
+	public static final char code2 = 'E';
+	public static final String course2 = "Education";
+	public static final char code3 = 'M';
+	public static final String course3 = "Mathematics";
+	public static final char code4 = 'B';
+	public static final String course4 = "Business";
+	public static final char code5 = 'N';
+	public static final String course5 = "Engineering";
+	public static final char code6 = 'A';
+	public static final String course6 = "Art";
 
-    // PRIVATE DATA
-    private int id;
-    private int score;
-    private char course;
+	// PRIVATE DATA
+	private int id;
+	private int score;
+	private char course;
 
-    // CONSTRUCTOR
-    public Student(int id, int score, char course) {
-        this.id = id;
-        this.score = score;
-        this.course = course;
-    }
+	// CONSTRUCTOR
+	public Student(int id, int score, char course) {
+		this.id = id;
+		this.score = score;
+		this.course = course;
+	}
 
-    // GETTERS
-    public int getId() {
-        return id;
-    }
+	// GETTERS
+	public int getId() {
+		return id;
+	}
 
-    public int getScore() {
-        return score;
-    }
+	public int getScore() {
+		return score;
+	}
 
-    public char getCourse() {
-        return course;
-    }
+	public char getCourse() {
+		return course;
+	}
 
-    // SETTERS - setCourse never used
-    public void setId(int id) {
-        this.id = id;
-    }
+	// SETTERS - setCourse never used
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setScore(int score) {
-        this.score = score;
-    }
+	public void setScore(int score) {
+		this.score = score;
+	}
 
-    // Converts code to full string, doesn't use private data so static
-    public static String convertCode(char code) {
-        if (code == code1) {
-            return course1;
-        } else if (code == code2) {
-            return course2;
-        } else if (code == code3) {
-            return course3;
-        } else if (code == code4) {
-            return course4;
-        } else if (code == code5) {
-            return course5;
-        } else if (code == code6) {
-            return course6;
-        } else {
-            return "Incorrect Course Code";
-        }
-    }
+	// Converts code to full string, doesn't use private data so static
+	public static String convertCode(char code) {
+		if (code == code1) {
+			return course1;
+		} else if (code == code2) {
+			return course2;
+		} else if (code == code3) {
+			return course3;
+		} else if (code == code4) {
+			return course4;
+		} else if (code == code5) {
+			return course5;
+		} else if (code == code6) {
+			return course6;
+		} else {
+			return "Incorrect Course Code";
+		}
+	}
 
-    // TOSTRING
-    public String toString() {
-        return "Id: " + getId() + "\nScore: " + getScore() + "\nCourse: " + convertCode(getCourse());
-    }
+	// TOSTRING
+	public String toString() {
+		return "Id: " + getId() + "\nScore: " + getScore() + "\nCourse: " + convertCode(getCourse());
+	}
 
 }
--- src/StudentArray.java
+++ src/StudentArray.java
@@ -6,183 +6,206 @@ import java.util.StringTokenizer;
 
 public class StudentArray {
 
-    // CONSTANTS
-    public static final int studentMax = 30;
-    public static final int gradeMin = 0;
-    public static final int gradeMax = 100;
+	// CONSTANTS
+	public static final int studentMax = 30;
+	public static final int gradeMin = 0;
+	public static final int gradeMax = 100;
 
-    // PRIVATE DATA
-    private Student[] array = new Student[studentMax];
-    private int filled = 0;
-    private double mean;
-    private double median;
-    private int highest;
-    private int lowest;
-    private int badDataLower;
-    private int badDataUpper;
+	// PRIVATE DATA
+	private Student[] array = new Student[studentMax];
+	private int filled = 0;
+	private double mean;
+	private double median;
+	private int highest;
+	private int lowest;
+	// Indexes of bad data, counter returned by method
+	private int leftBad;
+	private int rightBad;
 
-    // GETTERS
-    public double getMean() {
-        return mean;
-    }
+	// GETTERS
+	public int getFilled() {
+		return filled;
+	}
 
-    public double getMedian() {
-        return median;
-    }
+	public double getMean() {
+		return mean;
+	}
 
-    public int getHighest() {
-        return highest;
-    }
+	public double getMedian() {
+		return median;
+	}
 
-    public int getLowest() {
-        return lowest;
-    }
+	public int getHighest() {
+		return highest;
+	}
 
-    // NO ARGUMENT CONSTRUCTOR
-    public StudentArray() {
+	public int getLowest() {
+		return lowest;
+	}
 
-    }
+	// NO ARGUMENT CONSTRUCTOR
+	public StudentArray() {
 
-    // FILE READER
-    public void readFile() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter name of file, file must be in project directory\nEnter: ");
-        FileReader readFile = new FileReader(in.readLine());
-        BufferedReader inFile = new BufferedReader(readFile);
-        System.out.println("\nReading File...\n");
-        String input = inFile.readLine();
-        while (input != null) {
-            tokenizeString(input);
-            input = inFile.readLine();
-        }
-        inFile.close();
-    }
+	}
 
-    // Takes 3 values from string, inputs them into the student array
-    // called in file reader method
-    public void tokenizeString(String input) {
-        StringTokenizer st = new StringTokenizer(input);
-        for (; filled < array.length && st.hasMoreTokens(); filled++) {
-            array[filled] = new Student(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
-                    st.nextToken().charAt(0));
-        }
-    }
+	// FILE READER
+	public void readFile() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter name of file, file must be in project directory\nEnter: ");
+		FileReader readFile = new FileReader(in.readLine());
+		BufferedReader inFile = new BufferedReader(readFile);
+		System.out.println("\nReading File...\n");
+		String input = inFile.readLine();
+		while (input != null) {
+			tokenizeString(input);
+			input = inFile.readLine();
+		}
+		inFile.close();
+	}
 
-    // SORTING METHODS
-    // Sorts array by ID, Selection sort
-    public void sortId() {
-        for (int j = 0; j < filled - 1; j++) {
-            int minIndex = j;
-            for (int k = j + 1; k < filled; k++) {
-                if (array[k].getId() < array[minIndex].getId()) {
-                    minIndex = k;
-                }
-            }
-            int temp = array[j].getId();
-            array[j].setId(array[minIndex].getId());
-            array[minIndex].setId(temp);
-        }
-    }
+	// Takes 3 values from string, inputs them into the student array
+	// called in file reader method
+	public void tokenizeString(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		for (; filled < array.length && st.hasMoreTokens(); filled++) {
+			array[filled] = new Student(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+					st.nextToken().charAt(0));
+		}
+	}
 
-    // Sorts by score, Bubble sort
-    public void sortScore() {
-        int temp;
-        boolean swap = true;
-        int outer = filled;
-        while (swap) {
-            swap = false;
-            outer--;
-            for (int inner = 0; inner < outer; inner++) {
-                if (array[inner].getScore() > array[inner + 1].getScore()) {
-                    temp = array[inner].getScore();
-                    array[inner].setScore(array[inner + 1].getScore());
-                    array[inner + 1].setScore(temp);
-                    swap = true;
-                }
-            }
-        }
-    }
+	// SORTING METHODS
+	// Sorts array by ID, Selection sort
+	public void sortId() {
+		for (int j = 0; j < filled - 1; j++) {
+			int minIndex = j;
+			for (int k = j + 1; k < filled; k++) {
+				if (array[k].getId() < array[minIndex].getId()) {
+					minIndex = k;
+				}
+			}
+			int temp = array[j].getId();
+			array[j].setId(array[minIndex].getId());
+			array[minIndex].setId(temp);
+		}
+	}
 
-    // STATISTICS METHODS/OTHER
-    // Mean
-    public void findMean() {
-        int total = 0, counter = 0;
-        for (int i = badDataLower; i < filled - badDataUpper; i++) {
-            total += array[i].getScore();
-            counter++;
-        }
-        mean = (int) (((double) total / counter) * 100 + .5) / 100.0;
-    }
+	// Sorts by score, Bubble sort
+	public void sortScore() {
+		int temp;
+		boolean swap = true;
+		int outer = filled;
+		while (swap) {
+			swap = false;
+			outer--;
+			for (int inner = 0; inner < outer; inner++) {
+				if (array[inner].getScore() > array[inner + 1].getScore()) {
+					temp = array[inner].getScore();
+					array[inner].setScore(array[inner + 1].getScore());
+					array[inner + 1].setScore(temp);
+					swap = true;
+				}
+			}
+		}
+	}
 
-    // Median
-    public void findMedian() {
-        if (filled - (badDataLower + badDataUpper) % 2 == 0) {
+	// STATISTICS METHODS/OTHER
+	// Mean
+	public void findMean() {
+		int total = 0, counter = 0;
+		for (int i = leftBad; i <= rightBad; i++) {
+			total += array[i].getScore();
+			counter++;
+		}
+		mean = (int) (((double) total / counter) * 100 + .5) / 100.0;
+	}
 
-        } else {
+	// Median
+	public void findMedian() {
+		int length = rightBad - leftBad + 1;
+		if (length % 2 == 0) {
+			median = (array[leftBad + length / 2].getScore() + array[rightBad - length / 2].getScore()) / 2;
+		} else {
+			median = array[leftBad + length / 2].getScore();
+		}
+	}
 
-        }
-    }
+	// Highest/Lowest Score
+	public void findExtrema() {
+		lowest = array[leftBad].getScore();
+		highest = array[rightBad].getScore();
+	}
 
-    // Highest/Lowest Score
-    public void findExtrema() {
-        lowest = array[badDataLower].getScore();
-        highest = array[filled - 1 - badDataUpper].getScore();
-    }
+	// To be called after array is sorted by score, calls checkScore
+	public int checkScores() {
+		leftBad = 0;
+		rightBad = filled - 1;
+		int output = 0;
+		while (leftBad < filled && checkScore(array[leftBad].getScore())) {
+			leftBad++;
+			output++;
+		}
+		while (rightBad > leftBad && checkScore(array[rightBad].getScore())) {
+			rightBad--;
+			output++;
+		}
+		return output;
+	}
 
-    // To be called after array is sorted by score, calls checkScore
-    // Returns index as first value >=0, changes filled to first index <= 100
-    public void getPositions() {
-        badDataUpper = 0;
-        badDataLower = 0;
-        while (checkScore(array[badDataLower].getScore())) {
-            badDataLower++;
-        }
-        while (checkScore(array[filled - 1 - badDataUpper].getScore())) {
-            badDataUpper++;
-        }
-    }
+	// Returns false if given score is between 0 and 100
+	// Static for usage in other methods without an object
+	public static boolean checkScore(int score) {
+		return score > gradeMax || score < gradeMin;
+	}
 
-    // Returns false if given score is between 0 and 100
-    // Static for usage in other methods without an object
-    public static boolean checkScore(int score) {
-        return score > gradeMax || score < gradeMin;
-    }
+	// Percentage of each grade
+	public void gradePercents() {
+		// One position for each letter grade
+		int[] grades = new int[5];
+		int total = 0;
+		for (int i = leftBad; i <= rightBad; i++) {
+			if (array[i].getScore() >= 90) {
+				grades[0]++;
+			} else if (array[i].getScore() >= 80) {
+				grades[1]++;
+			} else if (array[i].getScore() >= 70) {
+				grades[2]++;
+			} else if (array[i].getScore() >= 65) {
+				grades[3]++;
+			} else {
+				grades[4]++;
+			}
+			total++;
+		}
+	}
 
-//    // Percentage of each grade
-//    public void gradePercents() {
-//        for
-//        if (score >= 90) {
-//        } else if (score >= 80) {
-//        } else if (score >= 70) {
-//        } else if (score >= 65) {
-//        } else {
-//        }
-//
-//    }
+	// Prints array, "arrayToString" to distinguish from toString in Student
+	public void arrayToString() {
+		for (int i = 0; i < filled; i++) {
+			System.out.println("\n" + array[i].toString());
+		}
+	}
 
-    // Prints array, "arrayToString" to distinguish from toString in Student
-    public void arrayToString() {
-        for (int i = 0; i < filled; i++) {
-            System.out.println("\n" + array[i].toString());
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        StudentArray roster = new StudentArray();
-        System.out.println("Program will read student's ids, scores, and course choices from a file"
-                + "\nMake sure it is properly formatted or the program will not work correctly"
-                + "\nIf your score is over 100 or below 0 it will not be included in statistics calculations but will still be printed");
-        roster.readFile();
-        roster.sortId();
-        System.out.println("Complete list - sorted by ID");
-        roster.arrayToString();
-        roster.sortScore();
-        roster.getPositions();
-        roster.findMean();
-        roster.findExtrema();
-        System.out.println("Mean: " + roster.getMean() +
-                "\nMedian: " + roster.getMedian() +
-                "\nHighest Score: " + roster.getHighest() +
-                "\nLowest Score: " + roster.getLowest());
-    }
+	public static void main(String[] args) throws IOException {
+		StudentArray roster = new StudentArray();
+		System.out.println("Program will read student's ids, scores, and course choices from a file"
+				+ "\nMake sure it is properly formatted or the program will not work correctly"
+				+ "\nIf your score is over 100 or below 0 it will not be included in statistics calculations but will still be printed");
+		roster.readFile();
+		roster.sortId();
+		System.out.println("Complete list - sorted by ID");
+		roster.arrayToString();
+		roster.sortScore();
+		int badData = roster.checkScores();
+		System.out.println("\nStatistics:");
+		System.out.println("Bad Data: " + badData);
+		if (badData != roster.getFilled()) {
+			roster.findMean();
+			roster.findExtrema();
+			roster.findMedian();
+			System.out.println("Mean: " + roster.getMean() + "\nMedian: " + roster.getMedian() + "\nHighest Score: "
+					+ roster.getHighest() + "\nLowest Score: " + roster.getLowest());
+		} else {
+			System.out.println("All scores are outside of 0 - 100, skipping statistics");
+		}
+	}
 }
