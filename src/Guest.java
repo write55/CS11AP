457be892b8f99d1133d77ee36417258d74aaf04b
write55 <42893610+write55@users.noreply.github.com>
2019-01-11 10:39:30 -0500

    Add files via upload

---
+++ src/Guest.java
@@ -0,0 +1,74 @@
+
+public class Guest {
+
+	private String firstName;
+	private String lastName;
+	private String company;
+	private String response;
+
+	// CONSTRUCTOR
+	public Guest(String firstName, String lastName, String company, String response) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.company = company;
+		this.response = response;
+	}
+
+	// GETTERS
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	// SETTERS
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+	// toString method
+	public String toString() {
+		return getLastName() + ", " + getFirstName() + getCompany() + getResponse();
+	}
+
+	public int compareGuests(Guest compare) {
+		// negative if referenced object is lexically lower
+		int firstValue = this.getFirstName().compareTo(compare.getFirstName());
+		int lastValue = this.getLastName().compareTo(compare.getLastName());
+		if (lastValue != 0) {
+			return lastValue;
+		} else if (firstValue != 0) {
+			return firstValue;
+		} else {
+			return 0;
+		}
+	}
+
+	public static void main(String[] args) {
+		Guest asdf = new Guest("Aaron", "Wu", "Aperture Science", "no");
+		Guest jkl = new Guest("Jorgen", "Wu", "Black Mesa", "yes");
+		System.out.println(asdf.compareGuests(jkl));
+	}
+}
---
+++ src/GuestList.java
@@ -0,0 +1,70 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class GuestList {
+
+	private ArrayList<Guest> guests;
+
+	public GuestList() {
+
+	}
+
+	public void readFile() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter relative path of file");
+		FileReader readFile = new FileReader(in.readLine());
+		BufferedReader inFile = new BufferedReader(readFile);
+		System.out.println("\nReading File...\n");
+		String input = inFile.readLine();
+		while (input != null) {
+			tokenizeString(input);
+			input = inFile.readLine();
+		}
+		inFile.close();
+	}
+
+	private void tokenizeString(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		// guests.add();
+
+	}
+
+	public void addGuest(Guest newGuest) {
+
+	}
+
+	public static void insertionSort(int[] elements) {
+		for (int j = 1; j < elements.length; j++) {
+			int temp = elements[j];
+			int possibleIndex = j;
+			while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) {
+				elements[possibleIndex] = elements[possibleIndex - 1];
+				possibleIndex--;
+			}
+			elements[possibleIndex] = temp;
+		}
+	}
+
+	public int binarySearch(Guest target) {
+		int left = 0;
+		int right = guests.size() - 1;
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			if (guests.get(middle).compareGuests(target) < 0) {
+				right = middle - 1;
+			} else if (guests.get(middle).compareGuests(target) > 0) {
+				left = middle + 1;
+			} else {
+				return middle;
+			}
+		}
+		return -1;
+	}
+
+	// integrate sorting and adding method, use binary search
+}
