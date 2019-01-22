47127c3bd2c3096fea82364830691e12eb9e9289
write55 <42893610+write55@users.noreply.github.com>
2019-01-22 11:25:22 -0500

    Add files via upload

--- src/Guest.java
+++ src/Guest.java
@@ -1,75 +1,79 @@
 
 public class Guest {
 
-    private String firstName;
-    private String lastName;
-    private String company;
-    private String response;
+	private String firstName;
+	private String lastName;
+	private String company;
+	private String response;
 
-    // CONSTRUCTOR
-    public Guest(String firstName, String lastName, String company, String response) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.company = company;
-        this.response = response;
-    }
+	// CONSTRUCTOR
+	public Guest(String firstName, String lastName, String company, String response) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.company = company;
+		this.response = response;
+	}
 
-    // GETTERS
-    public String getFirstName() {
-        return firstName;
-    }
+	// GETTERS
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getCompany() {
-        return company;
-    }
+	public String getCompany() {
+		return company;
+	}
 
-    public String getResponse() {
-        return response;
-    }
+	public String getResponse() {
+		return response;
+	}
 
-    // SETTERS
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	// SETTERS
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setCompany(String company) {
-        this.company = company;
-    }
+	public void setCompany(String company) {
+		this.company = company;
+	}
 
-    public void setResponse(String response) {
-        this.response = response;
-    }
+	public void setResponse(String response) {
+		this.response = response;
+	}
 
-    // toString method
-    public String toString() {
-        return "Name: " + getLastName() + ", " + getFirstName() + "\nCompany: " + getCompany() + "\nResponse: "
-                + getResponse();
-    }
+	// toString method
+	public String toString() {
+		String response = getResponse();
+		if (response.equals("?")) {
+			response = "maybe";
+		}
+		return "Name: " + getLastName() + ", " + getFirstName() + "\nCompany: " + getCompany() + "\nResponse: "
+				+ response;
+	}
 
-    public int compareGuests(Guest compare) {
-        // negative if referenced object is lexically lower than argument
-        int firstValue = this.getFirstName().compareTo(compare.getFirstName());
-        int lastValue = this.getLastName().compareTo(compare.getLastName());
-        if (lastValue != 0) {
-            return lastValue;
-        } else if (firstValue != 0) {
-            return firstValue;
-        } else {
-            return 0;
-        }
-    }
+	public int compareGuests(Guest compare) {
+		// negative if referenced object is lexically lower than argument
+		int firstValue = this.getFirstName().compareTo(compare.getFirstName());
+		int lastValue = this.getLastName().compareTo(compare.getLastName());
+		if (lastValue != 0) {
+			return lastValue;
+		} else if (firstValue != 0) {
+			return firstValue;
+		} else {
+			return 0;
+		}
+	}
 
-    public static void main(String[] args) {
-        Guest asdf = new Guest("Aaron", "Wu", "Aperture Science", "no");
-        Guest jkl = new Guest("Jorgen", "Wu", "Black Mesa", "yes");
-        System.out.println(asdf.compareGuests(jkl));
-    }
+	public static void main(String[] args) {
+		Guest asdf = new Guest("Aaron", "Wu", "Aperture Science", "no");
+		Guest jkl = new Guest("Jorgen", "Wu", "Black Mesa", "yes");
+		System.out.println(asdf.compareGuests(jkl));
+	}
 }
--- src/GuestList.java
+++ src/GuestList.java
@@ -8,84 +8,85 @@ import java.util.StringTokenizer;
 
 public class GuestList {
 
-    private ArrayList<Guest> guests;
+	private ArrayList<Guest> guests = new ArrayList<Guest>();
 
-    public GuestList() {
+	public GuestList() {
+	}
 
-    }
+	public void readFile() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter relative path of file: ");
+		FileReader readFile = new FileReader(in.readLine());
+		BufferedReader inFile = new BufferedReader(readFile);
+		System.out.println("\nReading File...\n");
+		String input = inFile.readLine();
+		while (input != null) {
+			tokenizeString(input);
+			input = inFile.readLine();
+		}
+		inFile.close();
+	}
 
-    public void readFile() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter relative path of file: ");
-        FileReader readFile = new FileReader(in.readLine());
-        BufferedReader inFile = new BufferedReader(readFile);
-        System.out.println("\nReading File...\n");
-        String input = inFile.readLine();
-        while (input != null) {
-            tokenizeString(input);
-            input = inFile.readLine();
-        }
-        inFile.close();
-    }
+	private void tokenizeString(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		while (st.hasMoreTokens()) {
+			guests.add(new Guest(st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()));
+		}
+	}
 
-    private void tokenizeString(String input) {
-        StringTokenizer st = new StringTokenizer(input);
-        while (st.hasMoreTokens()) {
-            addGuest(new Guest(st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()));
-        }
-    }
+	// Adds new guest in sorted order
+	public void addGuest(Guest newGuest) {
+		if (guests.indexOf(newGuest) == -1) {
+			System.out.println("Guest already on List\n" + guests.get(guests.indexOf(newGuest)).toString());
+			return;
+		}
+		int posIndex = 0;
+		while (posIndex < guests.size() && newGuest.compareGuests(guests.get(posIndex + 1)) > 0) {
+			posIndex++;
+		}
+		guests.add(posIndex, newGuest);
+	}
 
-    // Adds new guest in sorted order
-    public void addGuest(Guest newGuest) {
-        if (newGuest.getResponse().equals("?")) {
-            newGuest.setResponse("maybe");
-        }
-        int posIndex = 0;
-        while (posIndex < guests.size() && newGuest.compareGuests(guests.get(posIndex + 1)) > 0) {
-            posIndex++;
-        }
-        guests.add(posIndex, newGuest);
-    }
+	public void insertionSort() {
+		for (int j = 1; j < guests.size(); j++) {
+			Guest temp = guests.get(j);
+			int possibleIndex = j;
+			while (possibleIndex > 0 && temp.compareGuests(guests.get(possibleIndex - 1)) < 0) {
+				guests.set(possibleIndex, guests.get(possibleIndex - 1));
+				possibleIndex--;
+			}
+			guests.set(possibleIndex, temp);
+		}
+	}
 
-    public static void insertionSort(int[] elements) {
-        for (int j = 1; j < elements.length; j++) {
-            int temp = elements[j];
-            int possibleIndex = j;
-            while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) {
-                elements[possibleIndex] = elements[possibleIndex - 1];
-                possibleIndex--;
-            }
-            elements[possibleIndex] = temp;
-        }
-    }
+	public int binarySearch(Guest target) {
+		int left = 0;
+		int right = guests.size() - 1;
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			if (guests.get(middle).compareGuests(target) < 0) {
+				right = middle - 1;
+			} else if (guests.get(middle).compareGuests(target) > 0) {
+				left = middle + 1;
+			} else {
+				return middle;
+			}
+		}
+		return -1;
+	}
 
-    public int binarySearch(Guest target) {
-        int left = 0;
-        int right = guests.size() - 1;
-        while (left <= right) {
-            int middle = (left + right) / 2;
-            if (guests.get(middle).compareGuests(target) < 0) {
-                right = middle - 1;
-            } else if (guests.get(middle).compareGuests(target) > 0) {
-                left = middle + 1;
-            } else {
-                return middle;
-            }
-        }
-        return -1;
-    }
+	public void printList() {
+		for (Guest i : guests) {
+			System.out.println("\n" + i.toString());
+		}
+	}
 
-    public void printList() {
-        for (Guest i : guests) {
-            System.out.println(i.toString());
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		GuestList list = new GuestList();
+		list.readFile();
+		list.printList();
+		list.insertionSort();
+	}
 
-    public static void main(String[] args) throws IOException {
-        GuestList list = new GuestList();
-        list.readFile();
-        list.printList();
-    }
-
-    // integrate sorting and adding method, use binary search
+	// integrate sorting and adding method, use binary search
 }
