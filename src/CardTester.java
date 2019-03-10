18be96cef08fe527750df6ef61c00c62df3df92a
write55 <writeusernamehere55@gmail.com>
2019-03-10 13:03:53 -0400

    Updated Elevens Lab

--- src/CardTester.java
+++ src/CardTester.java
@@ -3,22 +3,21 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 * 
-	 * @param args
-	 *            is not used.
-	 */
-	public static void main(String[] args) {
-		Card first = new Card("S", "Ace", 2);
-		Card second = new Card("H", "Queen", 1);
-		Card third = new Card("H", "Queen", 1);
-		System.out.println(first.toString());
-		System.out.println(first.suit() + first.rank() + first.pointValue());
-		if (first.matches(second)) {
-			System.out.println("First and second are the same");
-		} else if (second.matches(third)) {
-			System.out.println(second.toString());
-		}
-	}
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        Card first = new Card("S", "Ace", 2);
+        Card second = new Card("H", "Queen", 1);
+        Card third = new Card("H", "Queen", 1);
+        System.out.println(first.toString());
+        System.out.println(first.suit() + first.rank() + first.pointValue());
+        if (first.matches(second)) {
+            System.out.println("First and second are the same");
+        } else if (second.matches(third)) {
+            System.out.println(second.toString());
+        }
+    }
 }
--- src/DeckTester.java
+++ src/DeckTester.java
@@ -3,63 +3,62 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 * 
-	 * @param args
-	 *            is not used.
-	 */
-	public static void main(String[] args) {
-		String[] ranks = { "jack", "queen", "king" };
-		String[] suits = { "blue", "red" };
-		int[] pointValues = { 11, 12, 13 };
-		Deck d = new Deck(ranks, suits, pointValues);
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] ranks = {"jack", "queen", "king"};
+        String[] suits = {"blue", "red"};
+        int[] pointValues = {11, 12, 13};
+        Deck d = new Deck(ranks, suits, pointValues);
 
-		System.out.println("**** Original Deck Methods ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Original Deck Methods ****");
+        System.out.println("  toString:\n" + d.toString());
+        System.out.println("  isEmpty: " + d.isEmpty());
+        System.out.println("  size: " + d.size());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deal a Card ****");
-		System.out.println("  deal: " + d.deal());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deal a Card ****");
+        System.out.println("  deal: " + d.deal());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deck Methods After 1 Card Dealt ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deck Methods After 1 Card Dealt ****");
+        System.out.println("  toString:\n" + d.toString());
+        System.out.println("  isEmpty: " + d.isEmpty());
+        System.out.println("  size: " + d.size());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deal Remaining 5 Cards ****");
-		for (int i = 0; i < 5; i++) {
-			System.out.println("  deal: " + d.deal());
-		}
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deal Remaining 5 Cards ****");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("  deal: " + d.deal());
+        }
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deck Methods After All Cards Dealt ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deck Methods After All Cards Dealt ****");
+        System.out.println("  toString:\n" + d.toString());
+        System.out.println("  isEmpty: " + d.isEmpty());
+        System.out.println("  size: " + d.size());
+        System.out.println();
+        System.out.println();
 
-		System.out.println("**** Deal a Card From Empty Deck ****");
-		System.out.println("  deal: " + d.deal());
-		System.out.println();
-		System.out.println();
+        System.out.println("**** Deal a Card From Empty Deck ****");
+        System.out.println("  deal: " + d.deal());
+        System.out.println();
+        System.out.println();
 
-		String[] ranks2 = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-		String[] suits2 = { "Spades", "Diamonds", "Clubs", "Hearts" };
-		int[] points2 = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-		Deck d2 = new Deck(ranks2, suits2, points2);
-		for (int i = 0; i < 5; i++) {
-			System.out.println(d2.toString());
-			d2.shuffle();
-		}
-	}
+        String[] ranks2 = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] suits2 = {"Spades", "Diamonds", "Clubs", "Hearts"};
+        int[] points2 = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        Deck d2 = new Deck(ranks2, suits2, points2);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(d2.toString());
+            d2.shuffle();
+        }
+    }
 }
