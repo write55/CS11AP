15885cf1344982cee34d538f0d393470300d7bbb
write55 <writeusernamehere55@gmail.com>
2019-03-12 16:27:46 -0400

    Updated Elevens Lab

--- src/ElevensBoard.java
+++ src/ElevensBoard.java
@@ -6,121 +6,118 @@ import java.util.ArrayList;
  */
 public class ElevensBoard extends Board {
 
-	/**
-	 * The size (number of cards) on the board.
-	 */
-	private static final int BOARD_SIZE = 9;
+    /**
+     * The size (number of cards) on the board.
+     */
+    private static final int BOARD_SIZE = 9;
 
-	/**
-	 * The ranks of the cards for this game to be sent to the deck.
-	 */
-	private static final String[] RANKS = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen",
-			"king" };
+    /**
+     * The ranks of the cards for this game to be sent to the deck.
+     */
+    private static final String[] RANKS = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen",
+            "king"};
 
-	/**
-	 * The suits of the cards for this game to be sent to the deck.
-	 */
-	private static final String[] SUITS = { "spades", "hearts", "diamonds", "clubs" };
+    /**
+     * The suits of the cards for this game to be sent to the deck.
+     */
+    private static final String[] SUITS = {"spades", "hearts", "diamonds", "clubs"};
 
-	/**
-	 * The values of the cards for this game to be sent to the deck.
-	 */
-	private static final int[] POINT_VALUES = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0 };
+    /**
+     * The values of the cards for this game to be sent to the deck.
+     */
+    private static final int[] POINT_VALUES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
 
-	/**
-	 * Flag used to control debugging print statements.
-	 */
-	private static final boolean I_AM_DEBUGGING = false;
+    /**
+     * Flag used to control debugging print statements.
+     */
+    private static final boolean I_AM_DEBUGGING = false;
 
-	/**
-	 * Creates a new <code>ElevensBoard</code> instance.
-	 */
-	public ElevensBoard() {
-		super(BOARD_SIZE, RANKS, SUITS, POINT_VALUES);
-	}
+    /**
+     * Creates a new <code>ElevensBoard</code> instance.
+     */
+    public ElevensBoard() {
+        super(BOARD_SIZE, RANKS, SUITS, POINT_VALUES);
+    }
 
-	/**
-	 * Determines if the selected cards form a valid group for removal. In
-	 * Elevens, the legal groups are (1) a pair of non-face cards whose values
-	 * add to 11, and (2) a group of three cards consisting of a jack, a queen,
-	 * and a king in some order.
-	 *
-	 * @param selectedCards
-	 *            the list of the indices of the selected cards.
-	 * @return true if the selected cards form a valid group for removal; false
-	 *         otherwise.
-	 */
-	@Override
-	public boolean isLegal(List<Integer> selectedCards) {
-		if (selectedCards.size() != 3) {
-			return containsPairSum11(selectedCards);
-		} else if (selectedCards.size() == 3) {
-			return containsJQK(selectedCards);
-		} else {
-			return false;
-		}
-	}
+    /**
+     * Determines if the selected cards form a valid group for removal. In
+     * Elevens, the legal groups are (1) a pair of non-face cards whose values
+     * add to 11, and (2) a group of three cards consisting of a jack, a queen,
+     * and a king in some order.
+     *
+     * @param selectedCards the list of the indices of the selected cards.
+     * @return true if the selected cards form a valid group for removal; false
+     * otherwise.
+     */
+    @Override
+    public boolean isLegal(List<Integer> selectedCards) {
+        if (selectedCards.size() == 2) {
+            return containsPairSum11(selectedCards);
+        } else if (selectedCards.size() == 3) {
+            return containsJQK(selectedCards);
+        } else {
+            return false;
+        }
+    }
 
-	/**
-	 * Determine if there are any legal plays left on the board. In Elevens,
-	 * there is a legal play if the board contains (1) a pair of non-face cards
-	 * whose values add to 11, or (2) a group of three cards consisting of a
-	 * jack, a queen, and a king in some order.
-	 *
-	 * @return true if there is a legal play left on the board; false otherwise.
-	 */
-	@Override
-	public boolean anotherPlayIsPossible() {
-		return containsPairSum11(cardIndexes()) || containsJQK(cardIndexes());
-	}
+    /**
+     * Determine if there are any legal plays left on the board. In Elevens,
+     * there is a legal play if the board contains (1) a pair of non-face cards
+     * whose values add to 11, or (2) a group of three cards consisting of a
+     * jack, a queen, and a king in some order.
+     *
+     * @return true if there is a legal play left on the board; false otherwise.
+     */
+    @Override
+    public boolean anotherPlayIsPossible() {
+        return containsPairSum11(cardIndexes()) || containsJQK(cardIndexes());
+    }
 
-	/**
-	 * Check for an 11-pair in the selected cards.
-	 *
-	 * @param selectedCards
-	 *            selects a subset of this board. It is list of indexes into
-	 *            this board that are searched to find an 11-pair.
-	 * @return true if the board entries in selectedCards contain an 11-pair;
-	 *         false otherwise.
-	 */
-	private boolean containsPairSum11(List<Integer> selectedCards) {
-		for (int i = 0; i < selectedCards.size() - 1; i++) {
-			for (int j = i + 1; j < selectedCards.size(); j++) {
-				if (cardAt(selectedCards.get(i)).pointValue() + cardAt(selectedCards.get(j)).pointValue() == 11) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    /**
+     * Check for an 11-pair in the selected cards.
+     *
+     * @param selectedCards selects a subset of this board. It is list of indexes into
+     *                      this board that are searched to find an 11-pair.
+     * @return true if the board entries in selectedCards contain an 11-pair;
+     * false otherwise.
+     */
+    private boolean containsPairSum11(List<Integer> selectedCards) {
+        for (int i = 0; i < selectedCards.size() - 1; i++) {
+            for (int j = i + 1; j < selectedCards.size(); j++) {
+                if (cardAt(selectedCards.get(i)).pointValue() + cardAt(selectedCards.get(j)).pointValue() == 11) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * Check for a JQK in the selected cards.
-	 *
-	 * @param selectedCards
-	 *            selects a subset of this board. It is list of indexes into
-	 *            this board that are searched to find a JQK group.
-	 * @return true if the board entries in selectedCards include a jack, a
-	 *         queen, and a king; false otherwise.
-	 */
-	private boolean containsJQK(List<Integer> selectedCards) {
-		boolean[] present = new boolean[3];
-		for (Integer i : selectedCards) {
-			if (cardAt(i).rank().equals("jack")) {
-				present[0] = true;
-			}
-			if (cardAt(i).rank().equals("queen")) {
-				present[1] = true;
-			}
-			if (cardAt(i).rank().equals("king")) {
-				present[2] = true;
-			}
-		}
-		for (boolean i : present) {
-			if (!i) {
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+     * Check for a JQK in the selected cards.
+     *
+     * @param selectedCards selects a subset of this board. It is list of indexes into
+     *                      this board that are searched to find a JQK group.
+     * @return true if the board entries in selectedCards include a jack, a
+     * queen, and a king; false otherwise.
+     */
+    private boolean containsJQK(List<Integer> selectedCards) {
+        boolean[] present = new boolean[3];
+        for (Integer i : selectedCards) {
+            if (cardAt(i).rank().equals("jack")) {
+                present[0] = true;
+            }
+            if (cardAt(i).rank().equals("queen")) {
+                present[1] = true;
+            }
+            if (cardAt(i).rank().equals("king")) {
+                present[2] = true;
+            }
+        }
+        for (boolean i : present) {
+            if (!i) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/ElevensGUIRunner.java
+++ src/ElevensGUIRunner.java
@@ -4,15 +4,14 @@
  */
 public class ElevensGUIRunner {
 
-	/**
-	 * Plays the GUI version of Elevens.
-	 * 
-	 * @param args
-	 *            is not used.
-	 */
-	public static void main(String[] args) {
-		Board board = new ElevensBoard();
-		CardGameGUI gui = new CardGameGUI(board);
-		gui.displayGame();
-	}
+    /**
+     * Plays the GUI version of Elevens.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        Board board = new ElevensBoard();
+        CardGameGUI gui = new CardGameGUI(board);
+        gui.displayGame();
+    }
 }
